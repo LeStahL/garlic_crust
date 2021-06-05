112e2331fde01881fa4671f2e2cc5e1f641de990
Alexander Kraus <nr4@z10.info>
2021-06-05 22:26:38 +0200

    fixed parser; works

--- src/knober/knober.rs
+++ src/knober/knober.rs
@@ -26,9 +26,10 @@ struct Edge {
 }
 
 enum AttributeType {
-	QuotedFileName,
+	String,
 	EnumEntry,
 	Float,
+	None,
 }
 
 struct Attribute {
@@ -74,15 +75,16 @@ fn main() {
 		.unwrap();
 
 	// Parse the synth definition file.
-	let mut name: String = String::from("");
+	let mut name: String = String::new();
 	let mut edges: Vec<Edge> = Vec::new();
 	let mut nodes: Vec<Node> = Vec::new();
 
-	for entry in parsed_synth_file.into_inner() {
-		match entry.as_rule() {
-			Rule::identifier => name = String::from(entry.as_str()),
+	for file_entry in parsed_synth_file.into_inner() {
+		match file_entry.as_rule() {
+			Rule::identifier => name = String::from(file_entry.as_str()),
 			Rule::block => {
-				for block_entry in entry.into_inner() {
+				// out_writer.write(String::from("Block:\n").as_bytes());
+				for block_entry in file_entry.into_inner() {
 					match block_entry.as_rule() {
 						Rule::node => {
 							let mut node_data_iterator = block_entry.into_inner();
@@ -90,55 +92,38 @@ fn main() {
 							let block_type = String::from(node_data_iterator.next().unwrap().as_str());
 							let mut attributes: Vec<Attribute> = Vec::new();
 							
+							// out_writer.write(&format!("Name: {}\n", &block_name).as_bytes());
+
 							for attribute_entry in node_data_iterator {
-								let mut attribute_name: String;
-								let attribute_type: AttributeType;
-								let attribute_value: String;
+								let mut attribute_data_iterator = attribute_entry.into_inner();
+								let attribute_name = String::from(attribute_data_iterator.next().unwrap().as_str());
 
-								match attribute_entry.as_rule() {
-									Rule::identifier => {
-										attribute_name = String::from(attribute_entry.as_str());
-										out_writer.write(&format!("Name: {}\n", attribute_name).as_bytes());
-									}
-									Rule::attribute_value => {
+								let mut attribute_type = AttributeType::None;
+								let mut attribute_value = String::new();
 
-									}
-									_ => (),
+								let attribute_value_data = attribute_data_iterator.next().unwrap();
+								match attribute_value_data.as_rule() {
+									Rule::string => {
+										attribute_type = AttributeType::String;
+									},
+									Rule::enum_entry => {
+										attribute_type = AttributeType::EnumEntry;
+									},
+									Rule::float => {
+										attribute_type = AttributeType::Float;
+									},
+									_ => ()
 								}
+								attribute_value = String::from(attribute_value_data.as_str());
+								
+								// out_writer.write(&format!("Attribute: {} - {}\n", &attribute_name, &attribute_value).as_bytes());
 
-								// attributes.push(Attribute{
-								// 	name: attribute_name,
-								// 	attribute_type: attribute_type,
-								// 	value: attribute_value,
-								// });
+								attributes.push(Attribute{
+									name: attribute_name,
+									attribute_type: attribute_type,
+										value: attribute_value,
+								});
 							}
-						// 	match node_data_iterator.next() {
-						// 		Some(x) => {
-						// 			for attribute_entry in x.into_inner() {
-						// 				let mut attribute_data_iterator = attribute_entry.into_inner();
-						// 				let attribute_name = String::from(attribute_data_iterator.next().unwrap().as_str());
-
-						// 				out_writer.write(&format!("Attribute name: {}\n", attribute_name).as_bytes());
-
-						// // 				let mut attribute_type: AttributeType;
-						// // 				let mut attribute_value: String;
-
-						// // 				match attribute_data_iterator.next().unwrap().as_rule() {
-						// // 					Rule::quoted_file_name => {
-						// // 						attribute_type = AttributeType::QuotedFileName;
-						// // 					}
-						// // 					Rule::enum_entry => {
-						// // 						attribute_type = AttributeType::EnumEntry;
-						// // 					}
-						// // 					Rule::float => {
-						// // 						attribute_type = AttributeType::Float;
-						// // 					}
-						// // 					_ => (),
-						// // 				}
-						// 			}
-						// 		},
-						// 		None => (),
-						// 	}
 
 							nodes.push(Node {
 								name: block_name,
